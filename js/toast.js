bf08592c1ffd9da110a4665ef08ef4094d111fa0
unknown <nehalrocksshetty@gmail.com>
2019-02-19 10:11:50 +0530

    'toast2'

--- js/toast.js
+++ js/toast.js
@@ -1,14 +1,39 @@
 let toastO=document.getElementById('toastO');
 let toastI=document.getElementById('toastI');
-
-let toastMessage=(message,backgroundColor)=>{
-    if(backgroundColor){
-        toastI.style.background=backgroundColor;
-    }
-    toastI.innerHTML=message;
-    toastO.style.display='flex';
+let toastCreate=(val)=>{
+    initToast(val);
+    setTextToast(val.text);
+}
+let setTextToast=(val2)=>{
+    toastI.innerHTML=val2;
+}
+let initToast=(val)=>{
+    let timer;
+    document.getElementById(val.id).addEventListener('click',()=>{
+        toastO.style.display='flex';
+        if(val.time){
+        timer=val.time*1000;}
+        else{
+            timer=10000;
+        }
     setTimeout(()=>{
         toastO.style.display='none';
-        console.log('none');
-    },5000)
+        console.log(timer);
+    },timer);
+    })
 }
+// let toastMessage=(message,backgroundColor)=>{
+//     if(backgroundColor){
+//         toastI.style.background=backgroundColor;
+//     }
+//     setTimeout(()=>{
+//         toastO.style.display='none';
+//         console.log('none');
+//     },5000)
+// }
+
+let myToastFun={};
+myToastFun.id='toast1';
+myToastFun.time=20;
+myToastFun.text='Toast No 1';
+toastCreate(myToastFun);
